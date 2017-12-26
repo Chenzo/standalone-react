a040120a56f61b634b0d4bfa694c8dd31206d440
Vince Suriani <chenzorama@hotmail.com>
2017-12-26 15:43:30 -0500

    Ah... more better

--- src/js/two.js
+++ src/js/two.js
@@ -36,3 +36,73 @@ ReactDOM.render(
     <Fourapp />,
     document.getElementById('four')
   );
+
+
+
+
+
+
+
+
+  /* five */
+  function Clock(props) {
+    return (
+      <div>
+        <h4>FIVE</h4>
+        <h3>It is <span className="dynamic">{props.date.toLocaleTimeString()}.</span></h3>
+      </div>
+    );
+  }
+  
+  function tick() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('five')
+    );
+  }
+  
+  tick();
+  setInterval(tick, 1000);
+
+
+
+
+  class Betterclock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+      return (
+        <div>
+            <h4>SIX</h4>
+            <h3>It is <span className="dynamic">{this.state.date.toLocaleTimeString()}.</span></h3>
+        </div>
+      );
+    }
+  }
+
+
+ReactDOM.render(
+    <Betterclock />,
+    document.getElementById('six')
+);
+
--- www/js/scripts.js
+++ www/js/scripts.js
@@ -197,6 +197,14 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 })("undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof self ? self : undefined);
 "use strict";
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 function Threeprop(props) {
     return React.createElement(
         "p",
@@ -235,3 +243,98 @@ function Fourapp() {
 }
 
 ReactDOM.render(React.createElement(Fourapp, null), document.getElementById('four'));
+
+/* five */
+function Clock(props) {
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h4",
+            null,
+            "FIVE"
+        ),
+        React.createElement(
+            "h3",
+            null,
+            "It is ",
+            React.createElement(
+                "span",
+                { className: "dynamic" },
+                props.date.toLocaleTimeString(),
+                "."
+            )
+        )
+    );
+}
+
+function tick() {
+    ReactDOM.render(React.createElement(Clock, { date: new Date() }), document.getElementById('five'));
+}
+
+tick();
+setInterval(tick, 1000);
+
+var Betterclock = function (_React$Component) {
+    _inherits(Betterclock, _React$Component);
+
+    function Betterclock(props) {
+        _classCallCheck(this, Betterclock);
+
+        var _this = _possibleConstructorReturn(this, (Betterclock.__proto__ || Object.getPrototypeOf(Betterclock)).call(this, props));
+
+        _this.state = { date: new Date() };
+        return _this;
+    }
+
+    _createClass(Betterclock, [{
+        key: "componentDidMount",
+        value: function componentDidMount() {
+            var _this2 = this;
+
+            this.timerID = setInterval(function () {
+                return _this2.tick();
+            }, 1000);
+        }
+    }, {
+        key: "componentWillUnmount",
+        value: function componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+    }, {
+        key: "tick",
+        value: function tick() {
+            this.setState({
+                date: new Date()
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "h4",
+                    null,
+                    "SIX"
+                ),
+                React.createElement(
+                    "h3",
+                    null,
+                    "It is ",
+                    React.createElement(
+                        "span",
+                        { className: "dynamic" },
+                        this.state.date.toLocaleTimeString(),
+                        "."
+                    )
+                )
+            );
+        }
+    }]);
+
+    return Betterclock;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Betterclock, null), document.getElementById('six'));
