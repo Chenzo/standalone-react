83cf49b77f127842c7885043eb3413625f0b25f2
Vince Suriani <chenzorama@hotmail.com>
2017-12-27 13:35:03 -0500

    need to use real data in a real situation

---
+++ src/js/three.js
@@ -0,0 +1,43 @@
+/* three.js */
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('seven')
+);
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number, i) =>
+  <li key={i}>{number}</li>
+);
+
+
+ReactDOM.render(
+  <ul>{listItems}</ul>,
+  document.getElementById('seven2')
+);
